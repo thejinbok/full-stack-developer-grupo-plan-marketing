6ed4e8bba280f3518831886eec86fa9ab7d76b49
Jin Bok <jin@quatredeux.com>
2023-07-30 12:31:42 -0300

    chore(server): create HouseholdApplianceController controller
    Closes #2.

---
+++ server/app/Http/Controllers/Api/V1/HouseholdApplianceController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\HouseholdAppliance;
+use Illuminate\Http\Request;
+
+class HouseholdApplianceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HouseholdAppliance $householdAppliance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HouseholdAppliance $householdAppliance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HouseholdAppliance $householdAppliance)
+    {
+        //
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\HouseholdApplianceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('v1/household-appliances', HouseholdApplianceController::class);

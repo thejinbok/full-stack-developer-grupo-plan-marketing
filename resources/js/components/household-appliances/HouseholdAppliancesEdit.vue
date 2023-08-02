36ab43791bbdd52a588df6c3572a6bd03ff492a7
Jin Bok <jin@quatredeux.com>
2023-08-02 06:32:03 -0300

    style: remove all uppercase transformations
    Refers to #12.

--- resources/js/components/household-appliances/HouseholdAppliancesEdit.vue
+++ resources/js/components/household-appliances/HouseholdAppliancesEdit.vue
@@ -35,7 +35,7 @@
             </div>
         </div>
 
-        <button type="submit" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md ring-gray-300 hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring disabled:opacity-25">
+        <button type="submit" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md ring-gray-300 hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring disabled:opacity-25">
             Save
         </button>
     </form>
--- resources/js/components/household-appliances/HouseholdAppliancesIndex.vue
+++ resources/js/components/household-appliances/HouseholdAppliancesIndex.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex mb-4 place-content-end">
         <div class="px-4 py-2 text-white bg-indigo-600 cursor-pointer hover:bg-indigo-700">
-            <router-link :to="{ name: 'household-appliances.create' }" class="text-sm font-medium uppercase">Create Household Appliance</router-link>
+            <router-link :to="{ name: 'household-appliances.create' }" class="text-sm font-medium">Create Household Appliance</router-link>
         </div>
     </div>
 
@@ -10,16 +10,16 @@
             <thead>
             <tr>
                 <th class="px-6 py-3 bg-gray-50">
-                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Name</span>
+                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500">Name</span>
                 </th>
                 <th class="px-6 py-3 bg-gray-50">
-                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Description</span>
+                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500">Description</span>
                 </th>
                 <th class="px-6 py-3 bg-gray-50">
-                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Voltage</span>
+                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500">Voltage</span>
                 </th>
                 <th class="px-6 py-3 bg-gray-50">
-                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase">Brand</span>
+                    <span class="text-xs font-medium leading-4 tracking-wider text-left text-gray-500">Brand</span>
                 </th>
                 <th class="px-6 py-3 bg-gray-50"></th>
             </tr>
@@ -41,8 +41,8 @@
                         {{ item.brand }}
                     </td>
                     <td class="px-6 py-4 text-sm leading-5 text-center text-gray-900 whitespace-no-wrap">
-                        <router-link :to="{ name: 'household-appliances.edit', params: { id: item.id } }" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 mr-2">Edit</router-link>
-                        <button @click="deleteHouseholdAppliance(item.id)" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25">
+                        <router-link :to="{ name: 'household-appliances.edit', params: { id: item.id } }" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 mr-2">Edit</router-link>
+                        <button @click="deleteHouseholdAppliance(item.id)" class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25">
             Delete</button>
                     </td>
                 </tr>
